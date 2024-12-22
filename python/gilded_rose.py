dba59fee08558f17c28554de989f88f1b1f6b522
MayBaMay <maylis.baschet@gmail.com>
2024-12-22 11:10:36 +0100

    Add helper methods to Item class for quality and sell_in management

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,38 @@
 # -*- coding: utf-8 -*-
 
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+
+class Item:
+    def __init__(self, name, sell_in, quality): 
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return f"{self.name}, {self.sell_in}, {self.quality}"
+
+    def update(self):
+        # This method must be overridden by subclasses to define specific update behavior.
+        raise NotImplementedError("Subclasses must override this method")
+
+    def increase_quality(self):
+        # Increase the quality of the item, ensuring it does not exceed MAX_QUALITY.
+        self.quality = min(self.quality + 1, MAX_QUALITY)
+
+    def decrease_quality(self):
+        # Decrease the quality of the item, ensuring it does not go below MIN_QUALITY.
+        self.quality = max(self.quality - 1, MIN_QUALITY)
+
+    def decrease_sell_in(self):
+        # Decrease the sell_in value by 1.
+        self.sell_in -= 1
+
+    def is_expired(self):
+        # Check if the item is expired (sell_in is less than 0).
+        return self.sell_in < 0
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -35,12 +68,3 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,12 +4,49 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+class ItemTest(unittest.TestCase):
+
+    def test_increase_quality(self):
+        item = Item("Test Item", 10, 10)
+        item.increase_quality()
+        self.assertEqual(item.quality, 11)
+        item.increase_quality()
+        self.assertEqual(item.quality, 12)
+        item = Item("Test max", 10, 49)
+        item.increase_quality()
+        self.assertEqual(item.quality, 50)
+        item.increase_quality()
+        self.assertEqual(item.quality, 50)
+
+    def test_decrease_quality(self):
+        item = Item("Test Item", 10, 10)
+        item.decrease_quality()
+        self.assertEqual(item.quality, 9)
+        item.decrease_quality()
+        self.assertEqual(item.quality, 8)
+        item = Item("Test min", 10, 1)
+        item.decrease_quality()
+        self.assertEqual(item.quality, 0)
+        item.decrease_quality()
+        self.assertEqual(item.quality, 0)
+
+    def test_decrease_sell_in(self):
+        item = Item("Test Item", 10, 10)
+        item.decrease_sell_in()
+        self.assertEqual(item.sell_in, 9)
+
+    def test_is_expired(self):
+        item = Item("Test Item", -1, 10)
+        self.assertTrue(item.is_expired())
+        item.sell_in = 0
+        self.assertFalse(item.is_expired())
+        item.sell_in = 10
+        self.assertFalse(item.is_expired())
+
+    def test_update_not_implemented(self):
+        item = Item("Test Item", 10, 10)
+        with self.assertRaises(NotImplementedError):
+            item.update()  
 
         
 if __name__ == '__main__':
